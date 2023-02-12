7b6b0b692df4553d87420c2d8e3daa25a7908491
Kiriejkee <evrum90@gmail.com>
2023-02-12 21:02:31 +0300

    DZ Task 2 with array

---
+++ Seminar1_DZ/Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿// Console.WriteLine("Введите первое число: ");
+// int firstNum = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите второе число: ");
+// int secondNum = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите третье число: ");
+// int thirdNum = Convert.ToInt32(Console.ReadLine());
+
+int[] maxOf = new int[3];
+
+Console.WriteLine("Введите первое число: ");
+maxOf[0] = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+maxOf[1] = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+maxOf[2] = Convert.ToInt32(Console.ReadLine());
+
+int ind = 0;
+int max = maxOf[0];
+
+while (ind <= 2)
+{
+    if (max < maxOf[ind])
+    {
+        max = maxOf[ind];
+    }
+    ind ++;
+}
+
+Console.WriteLine("Максимальное число: " + max);
