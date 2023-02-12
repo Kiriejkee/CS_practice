fd8fe884c9b1f84f09b79e363a8e62cd805bf211
Kiriejkee <evrum90@gmail.com>
2023-02-12 19:58:00 +0300

    tasks 1 and 2

---
+++ Seminar1/task1/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+Console.Write("Введите число, для которого мы ищем квадрат: ");
+
+int num = Convert.ToInt32(Console.ReadLine()); // Ридлайн читает строчку, необходимо конвертировать в Инт32 (работаем в основном в нем)
+
+int result = num * num;
+
+Console.WriteLine("Квадрат от числа: " + num + " - равен " + result);
---
+++ Seminar1/task2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+Console.Write("Ввдите первое число: ");
+
+int firstValue = Convert.ToInt32(Console.ReadLine()); //camelCase => firstValue, secondValue, firstNumberOfArray
+
+Console.Write("Ввдите второе число: ");
+
+int secondValue = Convert.ToInt32(Console.ReadLine());
+
+if (firstValue == Math.Pow(secondValue, 2)) // Math.Pow (число, степень)
+{
+    Console.WriteLine("Число " + firstValue + " является квадратом числа " + secondValue);
+}
+
+else
+{
+    Console.WriteLine("Число " + firstValue + " не является квадратом числа " + secondValue);
+}
