35c357341c4364fd1312d2a986e7c04a51958a74
Kiriejkee <evrum90@gmail.com>
2023-02-12 21:27:19 +0300

    DZ task 4

---
+++ Seminar1_DZ/Task4/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Введите число: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+int one = 1; 
+if (N > 1)
+{
+   while (one <= N)
+{
+    Console.WriteLine(one);
+    one += 2;
+} 
+}
+else
+{
+    if (N % 2 == 0)
+    {
+        while (N <= one)
+        {   Console.WriteLine(N);
+            N += 2;        
+        }  
+    }
+    else
+    {
+        N++;
+        while (N <= one)
+        {   Console.WriteLine(N);
+            N += 2;        
+        }  
+    }
+    
+}
