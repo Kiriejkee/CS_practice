6e4c444e137710a77847d24f981c59f6b7352820
Kiriejkee <evrum90@gmail.com>
2023-02-12 20:27:29 +0300

    task 3

---
+++ Seminar1/task3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число N:");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+int negativeN = N * (-1);
+
+if (N < 0)
+{
+    while(negativeN <= N)
+    {
+        Console.WriteLine(negativeN);
+        negativeN++;
+    }
+}
