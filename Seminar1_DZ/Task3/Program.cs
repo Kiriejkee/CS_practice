3238383871d648d6474efa54acffa076ec801009
Kiriejkee <evrum90@gmail.com>
2023-02-12 21:08:00 +0300

    DZ task 3

---
+++ Seminar1_DZ/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число для проверки на чётность: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Число " + num + " чётное!");
+}
+else
+{
+    Console.WriteLine("Число " + num + " НЕчётное!");
+}
