a0625e1025b169a890078463bed7787412d81438
Kiriejkee <evrum90@gmail.com>
2023-02-12 20:48:46 +0300

    DZ task 1

---
+++ Seminar1_DZ/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 2
+Console.Write("Введите первое число: ");
+
+int firstNum = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+
+int secondNum = Convert.ToInt32(Console.ReadLine());
+
+if (firstNum > secondNum)
+{
+    Console.WriteLine("Число " + firstNum + " больше числа " + secondNum);
+    Console.WriteLine("Число " + secondNum + " меньше числа " + firstNum);
+}
+else
+{
+    Console.WriteLine("Число " + secondNum + " больше числа " + firstNum);
+    Console.WriteLine("Число " + firstNum + " меньше числа " + secondNum);
+}
