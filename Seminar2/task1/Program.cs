18b4e732eee35cf7f1b5e201038f8421cf4a325a
Kiriejkee <evrum90@gmail.com>
2023-02-14 20:46:41 +0300

    задача на поиск наибольшей цифры в числе

---
+++ Seminar2/task1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача на поиск наибольшей цифры в числе
+
+//int number = new Random().Next(10,100);
+
+// int number = new Random().Next(11); // Первый параметр = 0. [0, 11)
+
+int number = new Random().Next(10, 100);
+
+int firstValue = number / 10; //первая цифра нашего числа
+
+int secondValue = number % 10; // Вторая цифра: 78 % 10 = 8 остаток (78 - 7*10 = 8)
+
+int max = firstValue;
+
+if (secondValue > max)
+{
+    max = secondValue;
+}
+
+Console.WriteLine("Наибольшая цифра в числе " + number + " равна " + max);
