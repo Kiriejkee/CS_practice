741bce88361ac8d69f18c41df08447ea04eabff0
Kiriejkee <evrum90@gmail.com>
2023-02-14 21:12:13 +0300

    задача с удалением второй цифры случайного трехзначного числа через строку

--- Seminar2/task2/Program.cs
+++ Seminar2/task2/Program.cs
@@ -4,11 +4,12 @@
 //123 -> 13
 //412 -> 42
 
-int number = new Random().Next(100,1000); // [100,999]
+// int number = new Random().Next(100,1000); // [100,999]
 
-// int firstValue = number / 100;
-// int thirdValue = number % 100 % 10;
+// int result = (number/100)*10 + number%10;
 
-int result = (number/100)*10 + number%10;
+// Console.WriteLine("При удалении второй цифры числа "+ number + " получается " + result);
 
-Console.WriteLine("При удалении второй цифры числа "+ number + " получается " + result);
+string message = Console.ReadLine(); // "456"
+
+Console.WriteLine(message[0] + "" + message[2]);
