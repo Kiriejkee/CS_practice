43d1323233c38a2db1534b1671de74d4999a6ada
Kiriejkee <evrum90@gmail.com>
2023-02-20 22:11:39 +0300

    Проверка пятизначного числа на палиндромность

---
+++ Seminar3_DZ/task1/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите пятизначное число: ");
+string num = Console.ReadLine();
+if (num.Length == 5)
+{
+    int numInt = 0;
+    bool isNum = int.TryParse(num, out numInt);
+    if( isNum == true)
+    {
+        int[] array = new int[5];
+        for (int i = 0; i < 5; i++)
+        {
+            array[i] = Convert.ToInt32(num[i]);
+        }
+        if (array[0] == array[4] && array [1] == array[3])
+        {
+            Console.WriteLine("Число является палиндромом");
+        }
+        else
+        {
+            Console.WriteLine("Число НЕ является палиндромом");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Введено не пятизначное число");
+    }
+}
+else
+{
+    Console.WriteLine("Введено не пятизначное число");
+}
