a14cad6312aecd626e00551393ab2ce29671c13d
Kiriejkee <evrum90@gmail.com>
2023-02-24 15:55:27 +0300

    mafsPower

---
+++ Seminar4_DZ/task1/Program.cs
@@ -0,0 +1,28 @@
+﻿int mafsPower(int num, int degree)
+{
+    int degreeIncrement = 1;
+    int result = num;
+    if(degree > 0)
+    {
+        while (degreeIncrement < degree)
+        {
+            result = result * num;
+            degreeIncrement++;
+        }
+        return result;
+    }
+    else
+    {
+      return 0;  
+    }
+    
+}
+
+
+Console.WriteLine("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Результатом возведения числа {A} в степень {B} будет: {mafsPower(A,B)}");
