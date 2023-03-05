81ff6bb8483932eb717eb52047d138a24d1ecc54
Kiriejkee <evrum90@gmail.com>
2023-03-05 13:43:23 +0300

    AdditionalTask

---
+++ Seminar6_DZ/task3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 42 (необязательно): Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+string TransformToBinary(int num)
+{
+    string binaryForm = "";
+    bool go = true;
+    while (go == true)
+    {
+        if (num / 2 != 1)
+        {
+            binaryForm = Convert.ToString(num % 2) + binaryForm;
+            num /= 2;
+            
+        }
+        else
+        {
+            binaryForm = "1" + Convert.ToString(num % 2) + binaryForm;
+            go = false;
+        }
+    }
+    return binaryForm;
+}
+
+Console.WriteLine("Введите число, которое необходимо трансормировать в двоичную запись > ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Двоичная форма записи введенного числа: {TransformToBinary(num)}");
