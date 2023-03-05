b811b50a2545c88717aee7fa06a563a3d586e680
Kiriejkee <evrum90@gmail.com>
2023-03-05 12:16:37 +0300

    task1

---
+++ Seminar6_DZ/task1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+//  Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3 //////4?
+
+void CountNum()
+{
+    string num;
+    bool isEmpty = false;
+    int countNum = 0;
+    Console.WriteLine($"Последовательно вводите числа или пустое значение, для остановки");
+    do 
+    {
+        num = Console.ReadLine();
+        isEmpty = String.IsNullOrEmpty(num);
+        if (isEmpty == false)
+        {
+            int convertNum = Convert.ToInt32(num);
+            if (convertNum > 0)
+            {
+                countNum++;
+            }
+        }
+
+    } while (isEmpty == false);
+    Console.WriteLine("Среди введенных вами чисел - " + countNum + " положительных");
+}
+
+
+
+CountNum();
