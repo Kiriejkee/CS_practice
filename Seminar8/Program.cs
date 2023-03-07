e422db3f545675ee778dd04bda66fd20ed57a8dc
Kiriejkee <evrum90@gmail.com>
2023-03-07 22:22:51 +0300

    Seminar8_2

--- Seminar8/Program.cs
+++ Seminar8/Program.cs
@@ -5,8 +5,7 @@
 // int max = int.MinValue;
 // int min = int.MaxValue;
 
-int[,] resultMatrix = GetMatrix(4, 4, 0, 10);
-PrintArray(resultMatrix);
+
 
 /// <summary>
 /// 
@@ -46,21 +45,88 @@ void PrintArray(int[,] inputMatrix)
 }
 
 /// <summary>
-/// Метод меняет первую и последнюю строчку местами
+/// Метод меняет две выбранные строчки местами
 /// </summary>
 /// <param name="inputMatrix">Входная целочисленная матрица</param>
-void ChangeRows(int[,] inputMatrix)
+void ChangeRows(int[,] inputMatrix, int startRow, int endRow)
 {
-    int lastRow = inputMatrix.GetLength(0) - 1;
     int temporaryRowElement;
     for (int i = 0; i < inputMatrix.GetLength(1); i++)
     {
-        temporaryRowElement=inputMatrix[0,i];
-        inputMatrix[0,i]=inputMatrix[lastRow,i];
-        inputMatrix[lastRow,i]=temporaryRowElement;
+        temporaryRowElement = inputMatrix[startRow - 1, i];
+        inputMatrix[startRow - 1, i] = inputMatrix[endRow - 1, i];
+        inputMatrix[endRow - 1, i] = temporaryRowElement;
     }
 }
 
-ChangeRows(resultMatrix);
-Console.WriteLine();
-PrintArray(resultMatrix);
+
+
+/// <summary>
+/// Создание перевернутой относительно главной диагонали матрицы
+/// </summary>
+/// <param name="inputMatrix">Исходная матрица</param>
+/// <returns>Перевернутая матрица</returns>
+int[,] GetTransMatrix(int[,] inputMatrix)
+{
+    int rows = inputMatrix.GetLength(0);
+    int cols = inputMatrix.GetLength(1);
+    int[,] copyMatrix = new int[rows, cols];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            copyMatrix[i, j] = inputMatrix[j, i];
+        }
+    }
+    return copyMatrix;
+}
+
+// int[,] resultMatrix = GetMatrix(4, 4, 0, 10);
+// PrintArray(resultMatrix);
+// ChangeRows(resultMatrix, 1, 2);
+// Console.WriteLine();
+// PrintArray(resultMatrix);
+
+// int[,] matr = GetTransMatrix(resultMatrix);
+// Console.WriteLine();
+// PrintArray(matr);
+
+
+
+int min = int.MaxValue;
+int indexMinRows = 0; // номер строчки с мин. элементом
+int indexMinCols = 0; // номер столбца с мин. элементом
+int rows = 4;
+int cols = 4;
+int[,] matrix = new int[rows, cols];
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        matrix[j, j] = new Random().Next(11);
+        Console.Write(matrix[j, j] + "\t");
+        if (min > matrix[i, j])
+        {
+            min = matrix[i, j];
+            indexMinRows = i;
+            indexMinCols = j;
+        }
+    }
+    Console.WriteLine();
+}
+Console.WriteLine($"Мин. элемент: {min}, \n строка: {indexMinRows}, \n столбец: {indexMinCols}");
+
+for (int i = 0; i < rows; i++)
+{
+    if(i != indexMinRows)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if (j != indexMinCols)
+            {
+                Console.Write(matrix[i,j] + "\t");
+            }
+        }
+        Console.WriteLine();
+    }
+}
