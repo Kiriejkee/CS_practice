58234d245eb64074bf7cfe7e47b4da96a0782c8e
Kiriejkee <evrum90@gmail.com>
2023-03-12 19:58:59 +0300

    Сумма натуральных элементов в промежутке

---
+++ Seminar9_DZ/task2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int M = 8;
+
+int N = 4;
+
+int SumBetween(int M, int N)
+{
+    int i = 1;
+    if (M == N) return M;
+    if (M >= N) i = -1;
+    return (M + SumBetween(M + i, N));
+}
+
+Console.WriteLine(SumBetween(M, N));
